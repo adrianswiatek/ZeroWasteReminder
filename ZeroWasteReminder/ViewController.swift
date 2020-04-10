fab82121c1611544ecac876e5a96e7d88775b8fb
Adrian Świątek <adrian@aswiatek.pl>
2020-04-10 23:05:41 +0200

    Initial Commit

---
+++ ZeroWasteReminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZeroWasteReminder
+//
+//  Created by Adrian Świątek on 10/04/2020.
+//  Copyright © 2020 Adrian Świątek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
