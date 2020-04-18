82926d40fb083eeaba17abdea0a4b0a4a6527f26
Adrian Świątek <adrian@aswiatek.pl>
2020-04-18 13:23:01 +0200

    Added ExpirationPeriodViewModel.

---
+++ ZeroWasteReminder/ViewModel/Add/ExpirationPeriodViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ExpirationPeriodViewModel.swift
+//  ZeroWasteReminder
+//
+//  Created by Adrian Świątek on 18/04/2020.
+//  Copyright © 2020 Adrian Świątek. All rights reserved.
+//
+
+import Foundation
