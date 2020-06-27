0b593501c8787b6e0ba6186965541ee866c58877
Adrian Świątek <adrian@aswiatek.pl>
2020-06-27 23:52:49 +0200

    Fixed items ordering on items list screen.

--- ZeroWasteReminder/Model/Entity/Expiration.swift
+++ ZeroWasteReminder/Model/Entity/Expiration.swift
@@ -5,6 +5,19 @@ public enum Expiration: Hashable {
     case date(_ date: Date)
 }
 
+extension Expiration: Equatable {
+    public static func == (lhs: Expiration, rhs: Expiration) -> Bool {
+        switch (lhs, rhs) {
+        case (.none, .none):
+            return true
+        case (.none, .date(_)), (.date(_), .none):
+            return false
+        case let (.date(lhsDate), .date(rhsDate)):
+            return Calendar.current.compare(lhsDate, to: rhsDate, toGranularity: .day) == .orderedSame
+        }
+    }
+}
+
 extension Expiration: Comparable {
     public static func < (lhs: Expiration, rhs: Expiration) -> Bool {
         switch (lhs, rhs) {
