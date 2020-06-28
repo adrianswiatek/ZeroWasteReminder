43134f5dce191023bd43ed3a2cfccf8fb0f69c82
Adrian Świątek <adrian@aswiatek.pl>
2020-06-29 00:32:33 +0200

    Retrieved sending refresh items result on main queue.

--- ZeroWasteReminder/Model/Service/ItemsService/CloudKitItemsService.swift
+++ ZeroWasteReminder/Model/Service/ItemsService/CloudKitItemsService.swift
@@ -64,11 +64,11 @@ public final class CloudKitItemsService: ItemsService {
             operation.recordFetchedBlock = { result.append($0) }
             operation.queryCompletionBlock = { _, error in
                 if let error = error {
-                    promise(.failure(ServiceError(error)))
+                    DispatchQueue.main.async { promise(.failure(ServiceError(error))) }
                 } else {
                     self?.itemsRepository.set(result.compactMap { self?.mapper.map($0).toItem() })
                     self?.cachedItemRecords = Set(result)
-                    promise(.success(()))
+                    DispatchQueue.main.async { promise(.success(())) }
                 }
             }
 
